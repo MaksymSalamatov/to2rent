b82f5010f9153444f77b26b752638cceb0293ff7
MaksymSalamatov <maksymsalamatov@gmail.com>
2021-02-05 17:56:47 +0100

    first commit

---
+++ #src/js/script.js
@@ -0,0 +1,11 @@
+const burger = document.querySelector('.burger');
+const menu = document.querySelector('.wrapper');
+const body = document.querySelector('body');
+
+
+
+burger.addEventListener('click', () => {
+  menu.classList.toggle('show');
+  burger.classList.toggle('show');
+  body.classList.toggle('no-scroll');
+})
